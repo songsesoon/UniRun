9d7e5f29cb3fbbfe2567114af0fbb15cf31cc32a
songsesoon <sss010201sss@naver.com>
2023-05-16 12:43:36 +0900

    2023.05.16
    dfgsdokgjsdgjsd

--- Uni-Run/Assets/Script/BackgroundLoop.cs
+++ Uni-Run/Assets/Script/BackgroundLoop.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
 
 // 왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
-public class BackgroundLoop : MonoBehaviour {
+public class BackgroundLoop : MonoBehaviour
+{
     private float width; // 배경의 가로 길이
 
-    private void Awake
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        // BoxCollider2D 컴포넌트의 Size 필드의 x  값을 가로 길이로 사용
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을 때 위치를 재배치
+        if (transform.position.x <= -width)
+        {
+            Reposition();
+        }
+    }
+
+    // 위치를 재배치하는 메서드
+    private void Reposition()
+    {
+        // 현재 위치에서 오른쪽으로 가로 길이*2 만큼 이동
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
     }
 }
