2bee581c5dbe58ceef5f1200a229f1603c4ba0f5
songsesoon <sss010201sss@naver.com>
2023-05-22 13:37:36 +0900

    2023.05.22
    gsdgsedghajga

--- Uni-Run/Assets/Script/ScrollingObject.cs
+++ Uni-Run/Assets/Script/ScrollingObject.cs
@@ -1,13 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 // 게임 오브젝트를 계속 왼쪽으로 움직이는 스크립트
 public class ScrollingObject : MonoBehaviour
 {
-    public float speed = 10f;//이동 속도
+    public float speed = 10f; // 이동 속도
 
+    // Update is called once per frame
     private void Update()
     {
-        // 초당 speed 의 속도로 왼쪽으로 평행이동
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        // 게임오버가 아니라면
+        if (!GameManager.instance.isGameover)
+        {
+            // 초당 speed의 속도로 왼쪽으로 평행이동
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
-}
